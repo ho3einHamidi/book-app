eaf8b26fcb3c25c8bb4df4118192e1a2e24f120d
ho3einHamidi <hosseinmhh@yahoo.com>
2024-05-19 14:25:40 +0330

    feat: heart icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import BookCard from "./Components/Book/BookCard";
-import Header from "./Components/Header/Header";
 import SearchBar from "./Components/SearchBar/SearchBar";
 import Layout from "./Layouts/Layout";
 function App() {
--- src/Components/Book/BookCard.jsx
+++ src/Components/Book/BookCard.jsx
@@ -1,5 +1,6 @@
 import styles from "./BookCard.module.css";
 import { books } from "../../Constants/mockData";
+import heartWhite from "../../assets/icons/heart-white.png";
 function Book() {
   return (
     <>
@@ -7,18 +8,23 @@ function Book() {
         {books.map((item) => {
           return (
             <div key={item.id} className={styles["book-card"]}>
-              <div className={styles.img}>
-                <img src={item.image} />
+              <div className={styles["right-side"]}>
+                <div className={styles.img}>
+                  <img src={item.image} />
+                </div>
+                <div className={styles.container}>
+                  <div className={styles.title}>
+                    <h3>{item.title}</h3>
+                    <h4>{item.author}</h4>
+                  </div>
+                  <div className={styles.details}>
+                    <p>{item.language}</p>
+                    <p>{item.pages} Pages</p>
+                  </div>
+                </div>
               </div>
-              <div className={styles.container}>
-                <div className={styles.title}>
-                  <h3>{item.title}</h3>
-                  <h4>{item.author}</h4>
-                </div>
-                <div className={styles.details}>
-                  <p>{item.language}</p>
-                  <p>{item.pages} Pages</p>
-                </div>
+              <div className={styles.heart}>
+                <img src={heartWhite}></img>
               </div>
             </div>
           );
